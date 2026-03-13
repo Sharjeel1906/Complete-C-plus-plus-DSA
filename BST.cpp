9152e83b4687fe04627a5775797e8d4a101023ea
sharjeel <sharjeelahmed1906@gmail.com>
2026-03-13 16:29:41 +0500

    new-entry

--- BST.cpp
+++ BST.cpp
@@ -9,18 +9,18 @@ public:
    Node* right;
    Node(int val){
     data = val;
-    left=right=NULL;
+    left = right = NULL;
    }
 };
 
-Node* insert(Node* root,int val){
-    if(root==NULL){
+Node* insert(Node* root, int val){
+    if(root == NULL){
         return new Node(val);
     }
-    if(val<root->data){
-        root->left = insert(root->left,val);
-    }else{
-        root->right = insert(root->right,val);
+    if(val < root->data){
+        root->left = insert(root->left, val);
+    } else {
+        root->right = insert(root->right, val);
     }
     return root;
 }
@@ -34,62 +34,59 @@ Node* buildBst(int arr[], int n) {
 }
 
 void inorder(Node* root){
-    if(root==NULL){
+    if(root == NULL){
         return;
     }
     inorder(root->left);
-    cout<<root->data<<" ";
+    cout << root->data << " ";
     inorder(root->right);
 }
 
-bool search(Node* root,int key){
-    if(root==NULL){
+bool search(Node* root, int key){
+    if(root == NULL){
         return false;
     }
-    if(root->data==key){
+    if(root->data == key){
         return true;
     }
-    if(root->data>key){
-        return search(root->left,key);
+    if(key < root->data){
+        return search(root->left, key);
+    } else {
+        return search(root->right, key);
     }
-    if(root->data<key){
-        return search(root->right,key);
-    }
-    return true;
 }
 
-// inorder successor  = Left most node in right subtree 
-// inorder successor has always one child or zero child
+// inorder successor = Left most node in right subtree
+// inorder successor has at most one child
 
 Node* inordersuccessor(Node* root){
-    while (root!=NULL && root->left !=NULL)
-    {
+    while (root != NULL && root->left != NULL){
         root = root->left;
     }
     return root;
-    
 }
 
-Node* Delete(Node* root,int key){
-    if(root==NULL){
+Node* Delete(Node* root, int key){
+    if(root == NULL){
         return NULL;
-    }else if(root->data>key){
-        root->left = Delete(root->left,key);
-    }else if(root->data<key){
-        root->right = Delete(root->right,key);
-    }else{
+    } else if(root->data > key){
+        root->left = Delete(root->left, key);
+    } else if(root->data < key){
+        root->right = Delete(root->right, key);
+    } else {
+        // Node to be deleted found
         if(root->left == NULL){
             Node* temp = root->right;
             delete root;
             return temp;
-        }else if(root->right==NULL){
+        } else if(root->right == NULL){
             Node* temp = root->left;
             delete root;
             return temp;
-        }else{
+        } else {
             Node* in_succ = inordersuccessor(root->right);
-            root->data =  in_succ->data;
-            root->right = Delete(root->right,in_succ->data);
+            root->data = in_succ->data;
+            root->right = Delete(root->right, in_succ->data);
         }
         return root;
     }
@@ -97,14 +94,21 @@ Node* Delete(Node* root,int key){
 }
 
 int main(){
-    int arr[6] = {3,2,1,5,6,4};
-    Node* root = buildBst(arr,6);
-    inorder(root);
-    cout<<endl;
+    int insertCount = 7; 
+    int insertArr[] = {5, 3, 7, 2, 4, 6, 8};
+    Node* root = buildBst(insertArr, insertCount);
 
-    //cout<<search(root,10);
-    Delete(root,5);
+    cout << "BST Inorder after insertion: ";
     inorder(root);
-    cout<<endl;
+    cout << endl;
 
+    int searchVal = 6;
+    cout << "Search " << searchVal << ": " << (search(root, searchVal) ? "Found" : "Not Found") << endl;
+
+    int deleteVal = 3;
+    root = Delete(root, deleteVal);
+
+    cout << "BST Inorder after deleting " << deleteVal << ": ";
+    inorder(root);
+    cout << endl;
 }
