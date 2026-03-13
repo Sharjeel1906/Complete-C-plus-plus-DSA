61637ac25927c4eb82fd11f750fb71c8b3a58ab0
sharjeel <sharjeelahmed1906@gmail.com>
2026-03-14 00:02:33 +0500

    Updated Graph

--- Graph.cpp
+++ Graph.cpp
@@ -1,56 +1,81 @@
-#include<iostream>
-#include<list>
-#include<queue>
+#include <iostream>
+#include <queue>
+#include <list>
+#include <vector>
 using namespace std;
-class Graph{
+
+class Graph
+{
     int V;
     list<int> *l;
+
 public:
-    Graph(int v){
-        V=v;
-        l = new list<int> [V];
+    Graph(int v)
+    {
+        V = v;
+        l = new list<int>[v];
     }
 
-    void addEdge(int u,int v){
+    void addEdge(int u, int v)
+    {
         l[u].push_back(v);
         l[v].push_back(u);
     }
 
-   void bfs(){
-    queue<int> q;
-    vector<bool> vis(V,false);
-    q.push(0);
-    vis[0] = true;
-    while(q.size()>0){
-        int u = q.front();
-        q.pop();
-        cout<<u<<" ";
-        for(int v : l[u]){
-            if(!(vis[v])){
-                vis[v] = true;
-                q.push(v);
+    void bfs(int start)
+    {
+        vector<bool> vis(V, false);
+        vector<int> distance(V, -1);
+        queue<int> q;
+
+        vis[start] = true;
+        distance[start] = 0;
+        q.push(start);
+
+        while (!q.empty())
+        {
+            int val = q.front();
+            q.pop();
+            cout << val << " (Distance: " << distance[val] << ")" << endl;
+
+            for (int neigh : l[val])
+            {
+                if (!vis[neigh])
+                {
+                    distance[neigh] = distance[val] + 1;
+                    vis[neigh] = true;
+                    q.push(neigh);
+                }
             }
-        }
-        cout<<endl;
-    }
-   }
-   
-    void print(){
-        for(int i=0;i<V;i++){
-            cout<<i<<":";
-            for(int neigh:l[i]){
-                cout<<neigh<<" ";
-            }
-             cout<<endl;
         }
     }
 };
-int main(){
-    Graph g(5);
-    g.addEdge(0,1);
-    g.addEdge(1,2);
-    g.addEdge(1,3);
-    g.addEdge(3,2);
-    g.addEdge(2,4);
-    g.bfs();
+
+int main()
+{
+    int vertices, edges;
+
+    cout << "Enter number of vertices: ";
+    cin >> vertices;
+
+    cout << "Enter number of edges: ";
+    cin >> edges;
+
+    Graph g(vertices+1); 
+
+    for (int i = 0; i < edges; i++)
+    {
+        int u, v;
+        cout << "Enter edge " << i + 1 << " From: ";
+        cin >> u;
+        cout << "To: ";
+        cin >> v;
+        g.addEdge(u, v);
+    }
+
+    int start;
+    cout << "Enter starting node for BFS: ";
+    cin >> start;
+
+    g.bfs(start);
 }
